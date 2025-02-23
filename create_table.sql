17744acc2c706c8bdbdf385281427338ed1dfe25
Zarif Hossain <zarifhossain808@gmail.com>
2025-02-23 13:23:56 +1100

    created more tables

--- create_table.sql
+++ create_table.sql
@@ -1,6 +1,20 @@
 CREATE TABLE User (
-    user_id int,
+    user_id INT,
     email VARCHAR(50) NOT NULL,
     contact_number VARCHAR(10) NOT NULL,
     PRIMARY KEY (user_id)
+);
+
+CREATE TABLE Vehicle (
+    vehicle_id INT,
+    make VARCHAR(50) NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    year VARCHAR(4) NOT NULL,
+    milage INT
+    PRIMARY KEY (vehicle_id)
+);
+
+CREATE TABLE Appointment (
+    appointment_id INT,
+    PRIMARY KEY (appointment_id)
 );
